8378b122de84b50ab2670be2fd524c3e0112cd70
Wes <wes.coffay@gmail.com>
2017-01-30 15:45:51 -0500

    basic endgame works

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ select disc
       '# of moves' counter ++
   winCondition check
     if towerSelected has 7 disc divs, alert 'You win in '# of moves'
+
+    Credit:
+    wood panel image
+    https://www.flickr.com/photos/katsura-relishtorelish/2789497086/in/photolist-5fuUim-68bXb-a3vayy-a3vayf-pFGqPZ-78NZ83-5fqwQx-5XsfuQ-5fqwNK-6DKsfB-b5B7w2-rxJ4GL-oc44NH-7xxhQG-68bXk-b5B7qT-oc58ur-otYeNV-b5B7Lz-5fuUgE-78K8ba-7VNeqU-78NZdQ-7QFEsE-bos2qc-dSWvTN-pLx5AG-b5B7Fp-5gorVp-dmCX5L-dSWvV7-8ABAqh-jVyoqt-5aMR5o-b3HsCX-b3HrXe-b3Hswz-mSBGF4-5YRocT-5aHzfX-oqtSeP-7QCkWp-cJp93L-qeVt8t-54Rh7C-cJp8SS-b3Hqfv-cBVp4C-qeLQrj-b3Hrcn accessed 1/30/17
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 //create the discs
 function createDiscs () {
   $("#firstTower").append (
-    '<div class="disc" id="disc1"></div>',
-    '<div class="disc" id="disc2"></div>',
-    '<div class="disc" id="disc3"></div>',
-    '<div class="disc" id="disc4"></div>',
-    '<div class="disc" id="disc5"></div>',
-    '<div class="disc" id="disc6"></div>',
-    '<div class="disc" id="disc7"></div>'
+    '<div class="disc selected" id="disc1"></div>',
+    '<div class="disc selected" id="disc2"></div>',
+    '<div class="disc selected" id="disc3"></div>',
+    '<div class="disc selected" id="disc4"></div>',
+    '<div class="disc selected" id="disc5"></div>',
+    '<div class="disc selected" id="disc6"></div>',
+    '<div class="disc selected" id="disc7"></div>'
   )
+  $("#firstTower>div.selected").removeClass("selected");
 }
 createDiscs()
 
@@ -20,39 +21,38 @@ function updateTime(){
   seconds++
   $('#timer').html(`Time Elapsed: ${seconds}`)
 }
-$(".disc").click(function(){
-  if(!isTimerRunning){
-    timerId = setInterval(updateTime, 1000)
-    isTimerRunning = true
-  }
-})
 
 //select disc once clicked
 isSelected = false
-// function selectCheck () {
-//   if ($(".board").find(".selected") === []) {
-//     isSelected = false
-//   }
-// }
-$(".disc").click(function () {
-  // console.log(isSelected)
+// $(".disc").click(function () { --this doesn't work
+$(document).on('click', '.disc', function() {
   if ((!isSelected) && ($(this).css('order') < ($(this).siblings('.disc').css('order')))) {
       isSelected = true
       $(this).addClass('selected')
-  }
-  else {
+      if(!isTimerRunning){
+        timerId = setInterval(updateTime, 1000)
+        isTimerRunning = true
+      }
+  }  else {
     $('.selected').removeClass('selected')
     isSelected = false
   }
 }
 )
-//select tower
+//select tower, compare selected to top disc
 var selectedTower
   $(".tower").click(function () {
   selectedTower = $(this)
   if ($(this).find('.disc').css('order') > $('*').find('.selected').css('order')) {
-    console.log('bam!')
     $(this).prepend($('.selected'))
     $('.selected').removeClass('selected')
+    isSelected = false
+    winTest()
   }
 })
+function winTest() {
+
+  if((document.querySelectorAll('#secondTower .disc').length === 8 ) || (document.querySelectorAll('#thirdTower .disc').length === 8 )){
+    alert(`You won in ${seconds}!`)
+  }
+}
